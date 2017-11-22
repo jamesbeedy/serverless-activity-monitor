68a5e6c141eb89e3936197d3d4c3ab73604f8a3c
jamesbeedy <jamesbeedy@gamil.com>
2017-11-22 08:04:17 -0800

    remove unneeded mocking

--- tests/00_test_activities.py
+++ tests/00_test_activities.py
@@ -41,76 +41,56 @@ def generate_random_string():
     return ''.join(gen_chars())
 
 
-class TestActivitiesEndpoint(object):
-    @classmethod
-    def setup_class(cls):
-        cls.mock_get_patcher = \
-            patch('serverless_activity_monitor.cli.activities_lib')
-        cls.mock_get = cls.mock_get_patcher.start()
+def test_create_new_activity_type_pass():
+    """This test is intended to test that the creation of a non-preexisting
+    activity_type succeeds.
+    """
+    
+    # Generate a new activity_type that *should* not be in the database
+    new_activity_type = 'activity-{}'.format(generate_random_string())
+    
+    # Check/ensure that new_activity_type doesn't already exist
+    existing_activity_types = activity_type_list()
+    if new_activity_type in existing_activity_types:
+        return False
+    
+    # Create the new activity_type
+    create_new_activity_resp = activity_type_create(new_activity_type)
+    
+    # Assert the new activity exists in the database
+    assert_list_equal(sorted(activity_type_list()),
+                      sorted(existing_activity_types + [new_activity_type]))
+    
+def test_delete_activity_type_pass():
+    """This test is intended to test that the deletion of an
+    activity_type succeeds.
+    """
 
-    @classmethod
-    def teardown_class(cls):
-        cls.mock_get_patcher.stop()
-
-    def test_create_new_activity_type_pass(self):
-        """This test is intended to test that the creation of a non-preexisting
-        activity_type succeeds.
-        """
-        self.mock_get.return_value.ok = True
-
-        # Generate a new activity_type that *should* not be in the database
-        new_activity_type = 'activity-{}'.format(generate_random_string())
-
-        # Check/ensure that new_activity_type doesn't already exist
-        existing_activity_types = activity_type_list()
-        if new_activity_type in existing_activity_types:
-            return False
-
-        self.mock_get.return_value = Mock()
-        self.mock_get.return_value.json.return_value = \
-            existing_activity_types + [new_activity_type]
-
-        # Create the new activity_type
-        create_new_activity_resp = activity_type_create(new_activity_type)
-
-        # Assert the new activity exists in the database
-        assert_list_equal(sorted(activity_type_list()),
-                          sorted(existing_activity_types + [new_activity_type]))
-
-    def test_delete_activity_type_pass(self):
-        """This test is intended to test that the deletion of an
-        activity_type succeeds.
-        """
-        self.mock_get.return_value.ok = True
-
-        # Generate a new activity_type that *should* not be in the database
-        new_activity_type = 'activity-{}'.format(generate_random_string())
-
-        # Check/ensure that new_activity_type doesn't already exist
-        init_existing_activity_types = activity_type_list()
-        if new_activity_type in init_existing_activity_types:
-            return False
-
-        # Create the new activity_type
-        create_new_activity_resp = activity_type_create(new_activity_type)
-
-        # Verify the new activity_type exists
-        if not new_activity_type in activity_type_list():
-            return False
-
-        self.mock_get.return_value = Mock()
-        self.mock_get.return_value.json.return_value = init_existing_activity_types
-
-        delete_activity_resp = activity_type_delete(new_activity_type)
-
-        # Assert the new activity exists in the database
-        assert_equal(sorted(activity_type_list()),
-                     sorted(init_existing_activity_types))
-
-    def test_update_activity_type_pass(self):
-        """This test is intended to test that updating the activities
-        for an activity_type succeeds.
-        """
-
-        return True
-
+    # Generate a new activity_type that *should* not be in the database
+    new_activity_type = 'activity-{}'.format(generate_random_string())
+    
+    # Check/ensure that new_activity_type doesn't already exist
+    init_existing_activity_types = activity_type_list()
+    if new_activity_type in init_existing_activity_types:
+        return False
+    
+    # Create the new activity_type
+    create_new_activity_resp = activity_type_create(new_activity_type)
+    
+    # Verify the new activity_type exists
+    if not new_activity_type in activity_type_list():
+        return False
+    
+    delete_activity_resp = activity_type_delete(new_activity_type)
+    
+    # Assert the new activity exists in the database
+    assert_equal(sorted(activity_type_list()),
+                 sorted(init_existing_activity_types))
+    
+def test_update_activity_type_pass():
+    """This test is intended to test that updating the activities
+    for an activity_type succeeds.
+    """
+    
+    return True
+    
